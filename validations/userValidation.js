a521517381d888f7c277e717f4fea053f47b6967
manoel <piloukos45@gmail.com>
2024-10-06 22:00:57 -0300

    fix: schema create user

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -2,10 +2,13 @@ const { z } = require('zod');
 
 // Validação para o registro de usuário
 const registerValidation = z.object({
-  // name: z.string().min(1, 'Nome é obrigatório'),
+  name: z.string().min(1, 'Nome é obrigatório'),
+  phone: z.string().optional(), // Adicione aqui, se necessário
+  dataNasc: z.date().optional(), // Exemplo para data de nascimento
   email: z.string().email('Email inválido'),
   password: z.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
 });
+
 
 // Validação para o login de usuário
 const loginValidation = z.object({
